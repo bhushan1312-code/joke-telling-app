a68e5ec2a4bb66bb617ee8c48eced745efa57b12
Bhushan Avhad <bhushanavhad1312@gmail.com>
2025-09-18 16:00:22 +0530

    laughing project

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+
+const para = document.getElementById("para")
+const para1 = document.getElementById("puch")
+
+
+function hello(){
+
+    const pi = fetch("https://official-joke-api.appspot.com/random_joke");
+
+pi
+
+    .then(data=>data.json())
+
+
+
+    .then(realdata=>{
+
+        para.innerText= `setup  : ${realdata.setup}` ;
+         console.log(realdata.setup)
+        para1.innerText =`punchline : ${realdata.punchline}`;
+        console.log(realdata.punchline)
+
+
+    })
+
+    .catch(error=>console.log(error))
+
+    }
